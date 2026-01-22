4663009e1fbaf454cf5a30919fb6a8cb9f3d8a89
Meanchan-Dara <meanchandara1@gmail.com>
2026-01-22 09:39:06 +0700

    Update Route

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get("/show", [ProductController::class, 'index']);
+    Route::post("/add", [ProductController::class, 'store']);
 });
